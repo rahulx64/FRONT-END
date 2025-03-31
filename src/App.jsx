cde230ed2f78375a81901d38d9983b646651e9eb
rahul <rahulxyad@gmail.com>
2025-04-01 04:53:50 +0530

    d-1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Form from './components/form'
+import './App.css'
+
+function App() {
+
+  return (
+     <>
+      <h1>Welcome to React</h1>
+      <Form/>
+     </>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+const form = () => {
+     
+
+
+
+  return (
+   <> 
+      <h1>form</h1>
+   </>
+  )
+}
+
+export default form
